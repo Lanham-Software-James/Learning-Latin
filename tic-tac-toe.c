8dd513bb7dd7954c3b6826711a624220ad493c5c
Jim Lanham <jrl5748@psu.edu>
2023-06-05 14:33:48 -0400

    Pretty up print. Decide who goes first. User turns.

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void generateBoard(int rows, int columns, char board[rows][columns])
 {
@@ -12,7 +13,8 @@ void generateBoard(int rows, int columns, char board[rows][columns])
   }
 }
 
-void readUserName( char* user_name, int user_number ) {
+void readUserName( char* user_name, int user_number ) 
+{
   int scanf_success = -1;
 
   while (scanf_success <= 0)
@@ -31,16 +33,99 @@ void readUserName( char* user_name, int user_number ) {
   }
 } 
 
-void printBoard( char *board, int rows, int columns )
+void printBoard(int rows, int columns, char board[rows][columns])
 {
+  printf("\n   1   2   3 \n");
+
   for (int i = 0; i < rows; i++)
   {
+    printf("%d ", i+1);
     for (int j = 0; j < columns; j++)
     {
-      printf(" %c ", *(board+i+j));
+      if(j == (columns - 1)){
+        printf(" %c ", board[i][j]);
+      }
+      else
+      {
+        printf(" %c |", board[i][j]);
+      }
+      
+      
     }
-    printf("\n");
+    if(i == (rows - 1))
+    {
+      printf("\n\n");
+    }
+    else
+    {
+      printf("\n  ---|---|---\n");
+    }
   }
+}
+
+void playTurn( int rows, int columns, char board[rows][columns], int user1_turn, char* username )
+{
+  int x_coord = -1;
+  int y_coord = -1;
+  int scanf_success = -1;
+  int turn_success = -1;
+
+  while (turn_success < 0)
+  {
+    while (scanf_success <= 0)
+    {
+      printf("%s, please enter your x-coordinate: ", username);
+      scanf_success = scanf("%d", &x_coord);
+
+      if (scanf_success <= 0)
+      {
+        printf("There was a problem reading your x-coordinate. Please try again.\n");
+      }
+      else if (x_coord < 0 || x_coord > columns)
+      {
+        printf("Please enter a number between 1 and %d.\n", columns);
+        scanf_success = -1;
+      }
+    }
+
+    scanf_success = -1;
+
+    while (scanf_success <= 0)
+    {
+      printf("%s, please enter your y-coordinate: ", username);
+      scanf_success = scanf("%d", &y_coord);
+
+      if (scanf_success <= 0)
+      {
+        printf("There was a problem reading your y-coordinate. Please try again\n");
+      }
+      else if (y_coord < 0 || y_coord > rows)
+      {
+        printf("Please enter a number between 1 and %d.\n", rows);
+        scanf_success = -1;
+      }
+    }
+
+    if (board[y_coord - 1][x_coord - 1] != '-')
+    {
+      printf("This space has been played. Please try again.\n");
+    }
+    else if(user1_turn == 1)
+    {
+      board[y_coord - 1][x_coord - 1] = 'X';
+      turn_success = 1;
+    }
+    else
+    {
+      board[y_coord - 1][x_coord - 1] = 'O';
+      turn_success = 1;
+    }
+  }
+}
+
+void checkWin( int rows, int columns, char board[rows][columns], int* user1_turn, char* username )
+{
+  // *user1_turn = -1;
 }
 
 int main()
@@ -51,7 +136,9 @@ int main()
   char *user_name_1 = malloc( sizeof( char ) * 50 );
   char *user_name_2 = malloc( sizeof( char ) * 50 );
 
-  char (*board)[ROWS] = malloc( sizeof( char[ROWS][COLUMNS] ));
+  char board[ROWS][COLUMNS];
+
+  int user1_turn = -1;
 
   generateBoard(ROWS, COLUMNS, board);
 
@@ -60,9 +147,38 @@ int main()
   readUserName(user_name_1, 1);
   readUserName(user_name_2, 2);
 
-  
+  srand(time(0));
+  user1_turn = (rand() % 2);
+
+  if (user1_turn == 1)
+  {
+    printf("Player 1 (%s) gets to go first!\n", user_name_1);
+  }
+  else{
+    printf("Player 2 (%s) gets to go first!\n", user_name_2);
+  }
+
+  while(user1_turn >= 0)
+  {
+
+    printBoard(ROWS, COLUMNS, board);
+
+    if(user1_turn == 1) {
+      playTurn( ROWS, COLUMNS, board, user1_turn, user_name_1);
+      checkWin( ROWS, COLUMNS, board, &user1_turn, user_name_1);
+      user1_turn = 0;
+    }
+    else {
+      playTurn( ROWS, COLUMNS, board, user1_turn, user_name_2);
+      checkWin( ROWS, COLUMNS, board, &user1_turn, user_name_2); 
+      user1_turn = 1;
+    }
+  }
   
 
-  printBoard(*board, ROWS, COLUMNS);
+  
+
+  free(user_name_1);
+  free(user_name_2);
   return(0);
 }
