273111e1450c1c57b554817c4da04f331e85a215
Jim Lanham <jrl5748@psu.edu>
2023-06-05 12:14:54 -0400

    Generate board, read in user names, print board.

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,22 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void printBoard(char *board[])
+void generateBoard(int rows, int columns, char board[rows][columns])
 {
-  for (int i = 0; i < 3; i++)
+  for (int i = 0; i < rows; i++)
   {
-    printf("%s\n", *(board+i));
+    for(int j = 0; j < columns; j++)
+    {
+      board[i][j] = '-';
+    }
   }
-  
+}
+
+void readUserName( char* user_name, int user_number ) {
+  int scanf_success = -1;
+
+  while (scanf_success <= 0)
+  {
+    printf("Please enter player %i's username (max 50 char): ", user_number);
+    scanf_success = scanf("%s", user_name);
+
+    if (scanf_success <= 0)
+    {
+      printf("There was a problem reading your username. Please try again\n");
+    }
+    else 
+    {
+      printf("Your username is: %s\n", user_name);
+    }
+  }
+} 
+
+void printBoard( char *board, int rows, int columns )
+{
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns; j++)
+    {
+      printf("%c", *(board+i+j));
+    }
+    printf("\n");
+  }
 }
 
 int main()
 {
-  char row[] = {'-', '-', '-'};
-  char *board[] = {row, row, row};
+  const int ROWS = 3;
+  const int COLUMNS = 3;
+
+  char *user_name_1 = malloc( sizeof( char ) * 50 );
+  char *user_name_2 = malloc( sizeof( char ) * 50 );
+
+  char (*board)[ROWS] = malloc( sizeof( char[ROWS][COLUMNS] ));
+
+  generateBoard(ROWS, COLUMNS, board);
 
   printf("Welcome to Tic-Tac-Toe!\n");
+  
+  readUserName(user_name_1, 1);
+  readUserName(user_name_2, 2);
 
-  printBoard(board);
+  
+  
 
+  printBoard(*board, ROWS, COLUMNS);
   return(0);
 }
