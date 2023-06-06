3a4a115fde54340cdd7cb17cc9e28f4db76a523e
Jim Lanham <jrl5748@psu.edu>
2023-06-06 11:45:56 -0400

    Getting user input for main menu.

--- personal-diary/personal-diary.c
+++ personal-diary/personal-diary.c
@@ -20,8 +20,53 @@ int main()
   int main_control = 1;
   while (main_control == 1)
   {
-    printMainMenu();
-    main_control = 0;
+    int scanf_success = -1;
+    
+    //Loop controling main menu input
+    while (scanf_success <= 0)
+    {
+      int user_choice = -1;
+      char c;
+      printMainMenu();
+      scanf_success = scanf("%d", &user_choice);
+
+      switch (user_choice)
+      {
+        case 1: //Add Record
+          /* code */
+          break;
+        
+        case 2: //View Record
+          /* code */
+          break;
+
+        case 3: //Edit Record
+          /* code */
+          break;
+
+        case 4: //Delete Record
+          /* code */
+          break;
+
+        case 5: //Change Record Password
+          /* code */
+          break;
+
+        case 6: //Exit
+          main_control = 0;
+          printf("Goodbye!\n");
+          break;
+
+        default:
+          scanf("%c", &c); //Need this to prevent infinite loop due to scanf
+          printf("Please enter a valid choice (1-6)!");
+          break;
+      }
+    }
+    
+    
+
+    //main_control = 0;
   }
   
 
