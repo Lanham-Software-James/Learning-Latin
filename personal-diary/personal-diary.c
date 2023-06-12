12392e0ff57c063d13a9cfb0171ad17d3ad7d73b
Jim Lanham <jrl5748@psu.edu>
2023-06-12 17:07:16 -0400

    Getting user input for add record.

--- personal-diary/personal-diary.c
+++ personal-diary/personal-diary.c
@@ -1,7 +1,10 @@
 /*
 **  This is another program to strengthen my understanding of C.
 */
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 /*
  *  Function to display the main menu to the user. 
@@ -20,6 +23,67 @@ void printMainMenu()
   printf("EDIT PASSWORD:                    [5]\n");
   printf("EXIT:                             [6]\n\n");
   printf("ENTER YOUR CHOICE: ");
+}
+
+/*
+ * Function for adding a record.
+ * WIP
+ */
+void addRecord()
+{
+  time_t rawtime;
+  struct tm * timeinfo;
+  int scanf_success = -1;
+  char c;
+  int n;
+  char *firstname = malloc( sizeof(char) * 50);
+  char *lastname = malloc( sizeof(char) * 50);
+  char *password = malloc( sizeof(char) * 50);
+  char *diary_entry = malloc( sizeof(char) * 1000);
+
+  printf("\n*************************************\n");
+  printf("*          ADD RECORD MENU          *\n");
+  printf("*************************************\n\n");
+
+  time ( &rawtime );
+  timeinfo = localtime ( &rawtime );
+  printf("Current Date/Time is: %d %d %d %d:%d:%d\n", timeinfo->tm_mday,
+            timeinfo->tm_mon + 1, timeinfo->tm_year + 1900,
+            timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+
+  
+  //Loop controling first name input
+  printf("Enter your first name: ");
+  while (scanf(" %49[^\n]", firstname) < 1)
+  {
+    printf("\nThere was a problem. Please enter your first name: ");
+  }
+
+  //Loop controling last name input
+  printf("Enter your last name: ");
+  while (scanf(" %49[^\n]", lastname) < 1)
+  { 
+    printf("\nThere was a problem. Please enter your last name: ");
+  }
+
+  //Loop controling entry password
+  printf("Enter your entry password: ");
+  while (scanf(" %49[^\n]", password) < 1)
+  { 
+    printf("\nThere was a problem. Please enter diary entry: ");
+  }
+
+  //Loop controling Diary Entry
+  printf("Enter your diary entry: ");
+  while (scanf(" %999[^\n]", diary_entry) < 1)
+  { 
+    printf("\nThere was a problem. Please enter diary entry: ");
+  }
+
+  free(firstname);
+  free(lastname);
+  free(password);
+  free(diary_entry);
 }
 
 /*
@@ -44,7 +108,7 @@ int main()
       switch (user_choice)
       {
         case 1: //Add Record
-          /* code */
+          addRecord();
           break;
         
         case 2: //View Record
